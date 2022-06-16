3ff351766b27049d9d798a604860725986c276c7
fdg2112 <fdg2112@gmail.com>
2022-06-15 23:53:16 -0300

    MVC implementado

--- TP4/TP4.Logic/ShippersLogic.cs
+++ TP4/TP4.Logic/ShippersLogic.cs
@@ -11,10 +11,18 @@ namespace TP4.Logic
     {
         public void Add(Shippers newElement)
         {
-            if (string.IsNullOrEmpty(newElement.CompanyName)) throw new Exception("El campo Nombre de la empresa no puede estar vacio");
-            if (newElement.CompanyName.Length > 40 || newElement.Phone.Length > 24) throw new Exception("Uno o más campos han sobrepasado el límite de caracteres permitidos");
-            context.Shippers.Add(newElement);
-            context.SaveChanges();
+            try
+            {
+                if (string.IsNullOrEmpty(newElement.CompanyName)) throw new Exception("El campo Nombre de la empresa no puede estar vacio");
+                if (newElement.CompanyName.Length > 40 || newElement.Phone.Length > 24) throw new Exception("Uno o más campos han sobrepasado el límite de caracteres permitidos");
+                context.Shippers.Add(newElement);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw new Exception("No se ha podido agregar");
+            }
+            
         }
 
         public void Delete(int id)
@@ -33,10 +41,17 @@ namespace TP4.Logic
 
         public void Update(Shippers element)
         {
-            if(element.Phone.Length > 24) throw new Exception("Se ha sobrepasado el límite de caracteres permitidos");
-            var shippersUpdate = context.Shippers.Find(element.ShipperID);
-            shippersUpdate.Phone = element.Phone;
-            context.SaveChanges();
+            try
+            {
+                if (element.Phone.Length > 24) throw new Exception("Se ha sobrepasado el límite de caracteres permitidos");
+                var shippersUpdate = context.Shippers.Find(element.ShipperID);
+                shippersUpdate.Phone = element.Phone;
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw new Exception("No se ha podido modificar");
+            }
         }
 
         public bool Finded(int id)
---
+++ TP4/TP7.Web/Controllers/ShippersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP4.Logic;
+using TP4.Entities;
+
+namespace TP7.Web.Controllers
+{
+    public class ShippersController : Controller
+    {
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            var shippersLogic = new ShippersLogic();
+
+            List<Shippers> shippers = shippersLogic.GetAll();
+
+            return View(shippers);
+        }
+    }
+}
